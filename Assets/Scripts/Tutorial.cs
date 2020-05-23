32c0ae9b3bc47e8dc41accb4ff2af38a247ff387
William AbouSharaf <liams_1998@hotmail.com>
2020-05-24 00:42:37 +0300

    Add tutorial component + logic/fucntionality

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    public GameObject[] tutorials;
+    private int currentTutorialToShow = 0;
+
+    void Start()
+    {
+        foreach (var tutorial in tutorials)
+        {
+            tutorial.SetActive(false);
+        }
+        ShowFirstTutorial();
+    }
+
+    public void SwitchToNextTutorial()
+    {
+        if (currentTutorialToShow == tutorials.Length - 1)
+        {
+            Debug.LogWarning("The amount of tutorials are finished");
+            return;
+        }
+        HideTutorial();
+        currentTutorialToShow += 1;
+        ShowTutorial();
+    }
+
+    public void ShowFirstTutorial()
+    {
+        tutorials[0].SetActive(true);
+    }
+
+    private void ShowTutorial()
+    {
+        tutorials[currentTutorialToShow].SetActive(true);
+    }
+
+    private void HideTutorial()
+    {
+        tutorials[currentTutorialToShow].SetActive(false);
+    }
+}
